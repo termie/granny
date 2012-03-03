a8f327c07b2a7906cc01d7411774d892c7b3cc5d
termie <github@anarkystic.com>
2012-03-03 12:05:13 -0800

    nouninate

--- granny/core.py
+++ granny/core.py
@@ -1,4 +1,14 @@
+import re
+
 from granny import code_analyzer
+
+
+WHITESPACE = re.compile(r'[ \t]+')
+LEADING = re.compile(r'\n ')
+CODE_NOUNINATOR = re.compile(r'\s`.+`[\s$]'
+                             r'|\s\w+\.\w+[\s$]')
+
+
 
 class Analyzer(object):
   docstring_label = frozenset(('literal', 'string', 'doc'))
@@ -17,6 +27,13 @@ class Analyzer(object):
     o = []
     for label, obj in lexer:
       if set(label) >= self.docstring_label:
-        o.append(eval(obj))
+        o.append(self._normalize_string(eval(obj)))
 
     return o
+
+
+  def _normalize_string(self, s):
+    s = WHITESPACE.sub(' ', s)
+    s = LEADING.sub('\n', s)
+    s = CODE_NOUNINATOR.sub(' NOUN ', s)
+    return s
