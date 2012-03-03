92d6791b4e19a10a24d7c327d2402462d3f4410d
Alex Weeks <aweeks@derp.(none)>
2012-03-03 13:02:57 -0800

    Added docstring

--- granny/comprehend.py
+++ granny/comprehend.py
@@ -4,6 +4,9 @@ from math import sqrt
 h = Hyphenator("./hyph_en_CA.dic", left=0, right=0)
 
 def smog( sentences ):
+	"""
+		Returns the SMOG reading comprehension level of a list of up to 30 sentences.  If fewer than 30 sentences are provided, it scales those sentences linearly to simulate a 30-sentence sample.
+	"""
 	poly_words = 0
 
 	for s in sentences[0:30]:
